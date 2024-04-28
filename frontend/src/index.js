7cfb3f719e1a712d37d207498f17b5bcdfdb16ed
Y Nguyen-Zintgraff <nhny703@gmail.com>
2024-04-28 02:52:40 -0700

    Configured Firebase FireStore

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,3 +20,24 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyC6hUtiUKiaMCcoQCAiaR0M0b5wsLTvtqc",
+	authDomain: "pokket-c55b4.firebaseapp.com",
+	projectId: "pokket-c55b4",
+	storageBucket: "pokket-c55b4.appspot.com",
+	messagingSenderId: "462660689245",
+	appId: "1:462660689245:web:b9693a9e5315dab1915cb4",
+	measurementId: "G-2422PMHLG5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
