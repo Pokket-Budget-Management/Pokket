e6d7e2d55b56e8b3042562c975fade421524711c
Marvin Huynh <38261965+hthm00@users.noreply.github.com>
2024-05-01 12:57:11 -0700

    SCRUM-30 add export button to DashBoard check empty data before export style button

--- frontend/src/shared/ExportButton.js
+++ frontend/src/shared/ExportButton.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import XLSX from 'xlsx';
+import React from "react";
+import XLSX from "xlsx";
+import { Button } from "react-bootstrap";
 
 const ExportButton = ({ data, fileName, buttonText }) => {
-  const convertToExcel = (data) => {
-    const ws = XLSX.utils.json_to_sheet(data);
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Data');
-    const wbout = XLSX.write(wb, { type: 'blob', bookType: 'xlsx' });
-    return URL.createObjectURL(wbout);
-  };
+	const convertToExcel = (data) => {
+		const ws = XLSX.utils.json_to_sheet(data);
+		const wb = XLSX.utils.book_new();
+		XLSX.utils.book_append_sheet(wb, ws, "Data");
+		const wbout = XLSX.write(wb, { type: "blob", bookType: "xlsx" });
+		return URL.createObjectURL(wbout);
+	};
 
-  const handleDownload = () => {
-    const excelData = convertToExcel(data);
-    const link = document.createElement('a');
-    link.href = excelData;
-    link.download = fileName;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
+	const handleDownload = () => {
+		if (!data) {
+			return;
+		}
+		const excelData = convertToExcel(data);
+		const link = document.createElement("a");
+		link.href = excelData;
+		link.download = fileName;
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	};
 
-  return (
-    <button onClick={handleDownload}>{buttonText}</button>
-  );
+	return <Button onClick={handleDownload}>{buttonText}</Button>;
 };
 
 export default ExportButton;
