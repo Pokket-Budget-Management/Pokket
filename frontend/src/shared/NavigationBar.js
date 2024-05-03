d3b07621449b64d30932492721630028da132a73
Y Nguyen-Zintgraff <nhny703@gmail.com>
2024-05-02 17:18:32 -0700

    Add logo

--- frontend/src/shared/NavigationBar.js
+++ frontend/src/shared/NavigationBar.js
@@ -2,6 +2,7 @@ import { Navbar, Nav } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import logo from "./logo.png"; // import the logo
 
 const NavBar = () => {
 	const [user, setUser] = useState(null);
@@ -28,9 +29,18 @@ const NavBar = () => {
 
 	return (
 		<Navbar bg="light" expand="lg" className="flex-column h-100">
-			<Navbar.Brand>Pokket</Navbar.Brand>
+			<Navbar.Brand className="me-auto ms-5">
+				<img
+					src={logo}
+					width="47"
+					height="39"
+					className="d-inline-block align-top"
+					alt="Pokket Logo"
+				/>
+			</Navbar.Brand>
+
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
+			<Navbar.Collapse id="basic-navbar-nav" className="me-auto ms-5">
 				<Nav className="me-auto flex-column">
 					<Nav.Link href="/">Dashboard</Nav.Link>
 					<Nav.Link href="/transactions">Transactions</Nav.Link>
